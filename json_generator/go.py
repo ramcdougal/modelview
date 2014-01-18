7b3ec21beaa9387971f008307bf44f026aef43df
Robert A. McDougal <Robert A. McDougal@localhost>
2014-01-18 17:16:41 -0500

    go now successfully downloads the zips, unzips, compiles, runs the simulation, and then cleans up... it does not yet generate the json

--- json_generator/go.py
+++ json_generator/go.py
@@ -1,29 +1,32 @@
 import sys
 import os
+import urllib2
 from run_protocols import protocol
-from neuron import h
 
 p = sys.argv[1]
 id = int(p.split('_')[0])
 
 protocol = protocol[p]
 
-# TODO: this won't work because wget doesn't get the zip file name
-
-os.system('wget "http://senselab.med.yale.edu/ModelDB/eavBinDown.asp?o=%d&a=23&mime=application/zip"' % id)
+zip_file = urllib2.urlopen('http://senselab.med.yale.edu/ModelDB/eavBinDown.asp?o=%d&a=23&mime=application/zip' % id).read()
+with open('zipfile.zip', 'wb') as f:
+    f.write(zip_file)
 
 assert(protocol['compile'][0][: 3] == 'cd ')
 dir_name = protocol['compile'][0][3:]
-os.system('unzip %s.zip' % dir_name)
+os.system('unzip zipfile.zip')
 os.system('cp json_generator.py %s/' % dir_name)
-os.system('cd %s' % dir_name)
+os.chdir(dir_name)
 
 for command in protocol['compile']:
     os.system(command)
 
 assert(len(protocol['launch']) == 1)
 assert(protocol['launch'][0][:15] == 'nrngui -python ')
-load_file = protocol['launch'][15:]
+load_file = protocol['launch'][0][15:]
+
+# this import has to be done after the mod files have been compiled
+from neuron import h
 h.load_file(load_file)
 
 # TODO: add hook for fadvance that execfiles json_generator
@@ -32,7 +35,6 @@ for command in protocol['run']:
     exec(command)
 
 os.system('cp *.json ..')
-
-os.system('cd ..')
-os.system('rm -f %s.zip' % dir_name)
+os.chdir('..')
+os.remove('zipfile.zip')
 os.system('rm -fr %s' % dir_name)
